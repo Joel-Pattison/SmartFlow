4cde46acf45010914e2fc8e958f04d4f5ae5c951
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-01-20 20:15:24 +0000

    Add key listener class

---
+++ src/voice/key_listener.py
@@ -0,0 +1,35 @@
+from pynput import keyboard
+import threading
+
+from src.voice.voice import Voice
+
+
+class KeyListener:
+    def __init__(self, voice):
+        self.voice_key = 'r'
+        self.voice = voice
+        self.listener = None
+        self.listener_thread = threading.Thread(target=self.start_listening)
+        self.listener_thread.start()
+
+    def start_listening(self):
+        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        with self.listener:
+            self.listener.join()
+
+    def stop_listening(self):
+        if self.listener:
+            self.listener.stop()
+        print("Stopped listening for key presses")
+        self.listener_thread.join()
+
+    def on_press(self, key):
+        try:
+            if key.char == self.voice_key and self.voice:
+                self.voice.enable_recording()
+        except AttributeError:
+            pass
+
+    def on_release(self, key):
+        if key == keyboard.KeyCode.from_char(self.voice_key):
+            self.voice.disable_recording()
