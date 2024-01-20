e57a9471f3a6f4777130681a7a0e261aaa6f0c0d
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-01-20 17:10:52 +0000

    Populate microphone list

--- src/ui/controller/main_view_controller.py
+++ src/ui/controller/main_view_controller.py
@@ -1,7 +1,23 @@
+import pyaudio
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from src.ui.view.main_view import Ui_Form
 from qframelesswindow import FramelessWindow, FramelessMainWindow, StandardTitleBar
 import sys
+
+
+def get_microphone_list():
+    audio_interface = pyaudio.PyAudio()
+    info = audio_interface.get_host_api_info_by_index(0)
+    num_devices = info.get('deviceCount')
+
+    microphone_list = []
+    for i in range(0, num_devices):
+        if audio_interface.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels') > 0:
+            mic_name = audio_interface.get_device_info_by_host_api_device_index(0, i).get('name')
+            microphone_list.append(mic_name)
+
+    audio_interface.terminate()
+    return microphone_list
 
 
 class MainWindow(FramelessMainWindow, Ui_Form):
@@ -9,4 +25,8 @@ class MainWindow(FramelessMainWindow, Ui_Form):
         super().__init__()
         self.setupUi(self)
         self.send_loading_ring.hide()
+        self.populate_microphone_cmb()
+
+    def populate_microphone_cmb(self):
+        self.microphone_cmb.addItems(get_microphone_list())
 
