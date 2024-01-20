30dd2e6be8b1c057f55ede8a33feb79470277403
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-01-20 20:17:46 +0000

    initialise voice and key listener

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,9 @@
 from GTPInterface import run_conversation
 from src.ui.ui import init_ui
+from src.voice.voice import Voice
+from src.voice.key_listener import KeyListener
 
 if __name__ == "__main__":
-    init_ui()
+    global_voice = Voice()
+    global_key_listener = KeyListener(global_voice)
+    init_ui(global_key_listener)
