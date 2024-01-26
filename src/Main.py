4e8fe50b93de46318107514de99e89bf9e71e54a
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-01-26 20:02:20 +0000

    Move ui.py code into main.py

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,18 @@
 from GTPInterface import run_conversation
-from src.ui.ui import init_ui
 from src.voice.voice import Voice
 from src.voice.key_listener import KeyListener
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from src.ui.controller.main_view_controller import MainWindow
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+
     global_voice = Voice()
-    global_key_listener = KeyListener(global_voice)
-    init_ui(global_key_listener)
+    key_listener = KeyListener(global_voice)
+
+    app.aboutToQuit.connect(key_listener.stop_listening)
+    sys.exit(app.exec_())
