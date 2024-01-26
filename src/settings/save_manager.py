d307e268d24bf3cae41c4fcc1c0ead4b283a4189
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-01-26 20:44:41 +0000

    add getters and setters for save manager

--- src/settings/save_manager.py
+++ src/settings/save_manager.py
@@ -4,8 +4,8 @@ import pickle
 
 class SettingsManager:
     settings = {
-        "mic_index": "mic_index",
-        "mic_name": "mic_name",
+        "mic_index": None,
+        "mic_name": None,
     }
     save_file = "settings.save"
 
@@ -13,14 +13,28 @@ class SettingsManager:
         if os.path.isfile("settings.txt"):
             self.load_save()
         else:
-            self.create_save()
+            self.update_save()
 
     def load_save(self):
         print("Settings file found: loading...")
         with open(self.save_file, "rb") as file:
             self.settings = pickle.load(file)
 
-    def create_save(self):
+    def update_save(self):
         print("No settings file found: creating...")
         with open(self.save_file, "wb") as file:
             pickle.dump(self.settings, file)
+
+    def set_microphone_index(self, index):
+        self.settings["mic_index"] = index
+        self.update_save()
+
+    def get_microphone_index(self):
+        return self.settings["mic_index"]
+
+    def set_microphone_name(self, name):
+        self.settings["mic_name"] = name
+        self.update_save()
+
+    def get_microphone_name(self):
+        return self.settings["mic_name"]
