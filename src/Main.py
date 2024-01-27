5065218dc92a109a0f6f3dd916ba97bdc4f31ffa
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-01-27 20:21:47 +0000

    Small GPT interface refactor

--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,10 @@
-from GTPInterface import run_conversation
 from src.settings.settings_manager import SettingsManager
 from src.voice.voice import Voice
 from src.voice.key_listener import KeyListener
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication
 import sys
 from src.ui.controller.main_view_controller import MainWindow
+from src.llm.conversation import llm_conversation
 
 if __name__ == "__main__":
     settings_manager = SettingsManager()
@@ -14,6 +13,7 @@ if __name__ == "__main__":
     win = MainWindow(settings_manager)
     win.show()
 
+    llm_conversation = llm_conversation(settings_manager)
     global_voice = Voice(settings_manager)
     key_listener = KeyListener(global_voice, win, settings_manager)
 
