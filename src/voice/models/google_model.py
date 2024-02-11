61cb8f59ab74e168b62617a23ce9f2317197edd3
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-02-11 20:23:38 +0000

    Add handlers for voice models

--- src/voice/models/google_model.py
+++ src/voice/models/google_model.py
@@ -1,3 +1,24 @@
+import speech_recognition as sr
+
+
 class Google:
     def __init__(self):
-        self.model = "google"
+        self.recognizer = sr.Recognizer()
+
+    def recognize(self, audio_frames):
+        try:
+            audio_data = sr.AudioData(b''.join(audio_frames), 44100, 2)
+            text = self.recognizer.recognize_google(audio_data)
+            return text
+        except sr.UnknownValueError:
+            return "Google Speech Recognition could not understand audio"
+        except sr.RequestError:
+            return "Could not request results from Google Speech Recognition service"
+        except Exception as e:
+            return f"An error occurred: {e}"
+
+    def load_model(self):
+        pass
+
+    def unload_model(self):
+        pass
--- src/voice/models/whisper_model.py
+++ src/voice/models/whisper_model.py
@@ -1,4 +1,34 @@
+import torch
 import whisper
+import numpy as np
+import io
+import wave
+
 class Whisper:
-    def __init__(self):
-        self.model = whisper.load_model("large-v3", device="cuda")
+    def __init__(self, model_name):
+        self.model = None
+        self.model_name = model_name
+
+    def recognize(self, audio_frames):
+        wav_writer = wave.open("tmp.wav", 'wb')
+        wav_writer.setnchannels(1)
+        wav_writer.setsampwidth(2)
+        wav_writer.setframerate(44100)
+        wav_writer.writeframes(b''.join(audio_frames))
+        wav_writer.close()
+
+        # with wave.open("tmp.wav") as wf:
+        #     n_frames = wf.getnframes()
+        #     frame_data = wf.readframes(n_frames)
+        #     audio_data = np.frombuffer(frame_data, dtype=np.int16).astype(np.float32) / 32768.0
+
+        result = self.model.transcribe("tmp.wav")
+        return result["text"]
+
+
+    def load_model(self):
+        self.model = whisper.load_model(self.model_name, device="cuda")
+
+    def unload_model(self):
+        self.model = None
+        torch.cuda.empty_cache()
