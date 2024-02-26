cac2bc75bd1706cb22bc2eb87f38dad84ea1fe28
Joel Pattison <113473745+Joel-Pattison@users.noreply.github.com>
2024-02-26 10:56:40 +0000

    Send audio data to whisper model directly

--- src/voice/models/whisper_model.py
+++ src/voice/models/whisper_model.py
@@ -8,23 +8,15 @@ class Whisper:
     def __init__(self, model_name):
         self.model = None
         self.model_name = model_name
+        self.sample_rate = 16000
 
     def recognize(self, audio_frames):
-        wav_writer = wave.open("tmp.wav", 'wb')
-        wav_writer.setnchannels(1)
-        wav_writer.setsampwidth(2)
-        wav_writer.setframerate(44100)
-        wav_writer.writeframes(b''.join(audio_frames))
-        wav_writer.close()
+        all_frames_bytes = b''.join(audio_frames)
 
-        # with wave.open("tmp.wav") as wf:
-        #     n_frames = wf.getnframes()
-        #     frame_data = wf.readframes(n_frames)
-        #     audio_data = np.frombuffer(frame_data, dtype=np.int16).astype(np.float32) / 32768.0
+        audio_data = np.frombuffer(all_frames_bytes, dtype=np.int16).astype(np.float32) / 32768.0
 
-        result = self.model.transcribe("tmp.wav")
+        result = self.model.transcribe(audio_data)
         return result["text"]
-
 
     def load_model(self):
         self.model = whisper.load_model(self.model_name, device="cuda")
@@ -32,3 +24,6 @@ class Whisper:
     def unload_model(self):
         self.model = None
         torch.cuda.empty_cache()
+
+    def get_sample_rate(self):
+        return self.sample_rate
